9839687b85b69f61a62b72258ed0c6f5feaf8e1e
Brian Pineau <pinoub@gmail.com>
2020-06-01 22:01:54 -0500

    Begin set up of room filter

---
+++ src/components/RoomFilter.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useContext } from 'react';
+import { RoomContext } from '../context';
+import Title from '../components/Title';
+
+export default function RoomFilter() {
+  const context = useContext(RoomContext);
+  console.log(context);
+  return (
+    <div>
+      Hello from RoomFilter
+    </div>
+  )
+}
